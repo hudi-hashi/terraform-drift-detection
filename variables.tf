75dda547ec06b4727e1b2c75a1d5d35cc173d76b
Hudi <132931651+hudi-hashi@users.noreply.github.com>
2025-03-04 09:47:54 -0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -21,3 +21,8 @@ variable "public_subnet_cidrs" {
   type        = list(string)
   default     = ["10.0.10.0/24", "10.0.11.0/24"]
 }
+
+variable "instance_name" {
+  description = "EC2 instance name"
+  default     = "Provisioned by Terraform Cloud"
+}
